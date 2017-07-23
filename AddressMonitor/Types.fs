06c9749d60109da90584f1523e0b33be5cbc4c21
Anthony <ajlusardi@gmail.com>
2017-07-23 16:18:05 -0400

    Initial commit

---
+++ AddressMonitor/Types.fs
@@ -0,0 +1,24 @@
+﻿module Types
+
+type Network = ETC | ETH | BTC
+
+type Address = {
+    Network: Network;
+    Address: string;
+}
+
+type User = {
+    Address: Address[];
+    Email: string;
+}
+
+// Stubs, need to implement
+let validateEthereumAddress (network:Network) (x:string) : Address option = Some {Network=network; Address=x}
+
+let validateBitcoinAddress = validateEthereumAddress
+
+let validateAddress (network:Network) (x:string) : Address option =
+    match network with
+    | Network.ETC -> validateEthereumAddress network x
+    | Network.ETH -> validateEthereumAddress network x
+    | Network.BTC -> validateBitcoinAddress network x
