2563ce65d913a4ea9c5693e2dc440a8564b1f870
Anthony <ajlusardi@gmail.com>
2017-07-24 12:27:56 -0400

    Cleanup

--- AddressMonitor/View.fs
+++ AddressMonitor/View.fs
@@ -3,32 +3,30 @@ open Suave.Html
 open Suave.Xml
 open Suave.Form
 
-let divId id = divAttr["id", id]
-
+let divId id interior = divAttr["id", id] interior
 let aHref link label = tag "a" ["href", link] label
-
 let h1 = tag "h1" []
 
 let index container = xmlToString <|
-            html [
-                head [
-                    title "Address Monitor"
-                ]
+                        html [
+                            head [
+                                title "Address Monitor"
+                            ]
 
-                body [
-                    divId "header"[
-                        h1 (aHref Path.home (text "Address Monitor - Home"))
-                    ]
+                            body [
+                                divId "header"[
+                                    h1 (aHref Path.home (text "Address Monitor - Home"))
+                                ]
 
-                    divId "main" container
+                                divId "main" container
 
-                    divId "footer" [
-                        text "built with "
-                        aHref "http://fsharp.org" (text "F#")
-                        text " and "
-                        aHref "http://suave.io" (text "Suave.IO")
-                    ]            
-                ]
-            ]
+                                divId "footer" [
+                                    text "built with "
+                                    aHref "http://fsharp.org" (text "F#")
+                                    text " and "
+                                    aHref "http://suave.io" (text "Suave.IO")
+                                ]            
+                            ]
+                        ]
 
 let home = [ text "Home" ]
