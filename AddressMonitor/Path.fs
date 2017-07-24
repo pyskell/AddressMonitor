66be6712e8a75101e9c2d8f7f88fed28e46f9541
Anthony <ajlusardi@gmail.com>
2017-07-24 14:11:44 -0400

    At least the types make sense.

--- AddressMonitor/Path.fs
+++ AddressMonitor/Path.fs
@@ -1,3 +1,9 @@
 ﻿module Path
 
+open Types
+
+// TODO: Figure this out.
+type AddressPath = PrintfFormat<(Network -> Address -> string), unit, string, string, (Network * Address)>
+
 let home = "/"
+let addAddress : AddressPath = "/add/%A/%A"
--- AddressMonitor/Program.fs
+++ AddressMonitor/Program.fs
@@ -1,22 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-
-open Suave
+﻿open Suave
 open Suave.Successful
 open Suave.Filters
 open Suave.Operators
 open Suave.RequestErrors
+open Types
+open Suave.Xml
 
 let html interior = 
     OK (View.index interior)
     >=> Writers.setMimeType "text/html; charset=utf-8"
 
+let addAddress (network:Network, address:Address) =
+    warbler(fun _ -> html [text "Added album"])
+
 let webPart = choose [
                 path Path.home >=> html View.home
+                pathScan Path.addAddress addAddress
 ]
 
 [<EntryPoint>]
 let main argv = 
     startWebServer defaultConfig webPart
-    //printfn "%A" argv
     0 // return an integer exit code
