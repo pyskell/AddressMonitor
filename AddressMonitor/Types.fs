e75dc84d1155fda7ac53382d00527f689fd6b930
Anthony <ajlusardi@gmail.com>
2017-07-24 11:14:56 -0400

    Fix up validation functions

--- AddressMonitor/Types.fs
+++ AddressMonitor/Types.fs
@@ -1,5 +1,7 @@
 ﻿module Types
 
+open Microsoft.FSharp.Core
+
 type Network = ETC | ETH | BTC
 
 type Address = {
@@ -12,12 +14,20 @@ type User = {
     Email: string;
 }
 
-// Stubs, need to implement
-let validateEthereumAddress (network:Network) (x:string) : Address option = Some {Network=network; Address=x}
+let validateEthereumAddress (network:Network) (x:string) : bool = 
+    let len = String.length x
+    match len with
+    | 40 -> true
+    | _ -> false
 
-let validateBitcoinAddress = validateEthereumAddress
+let validateBitcoinAddress (network:Network) (x:string) : bool =
+    let len = String.length x
+    let first = x.Chars(0)
+    match (len, first) with
+    | (l,f) when l > 24 && l < 35 && f = '1' -> true
+    | _,_ -> false
 
-let validateAddress (network:Network) (x:string) : Address option =
+let validateAddress (network:Network) (x:string) : bool =
     match network with
     | Network.ETC -> validateEthereumAddress network x
     | Network.ETH -> validateEthereumAddress network x
